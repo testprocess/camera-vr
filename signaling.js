2dd63fc0057e5173a84e65d00d7ca178f4d604f5
Dipokal HHJ <hhj@devent.kr>
2024-01-19 20:52:14 +0900

    fix: remove consolelog

--- signaling.js
+++ signaling.js
@@ -1,35 +1,21 @@
 export async function socket (io) {
     io.on("connection", (socket) => {
-
-        console.log("E")
-
         socket.on("test", () => {
             console.log("OTT")
 
         })
 
-        
-
-
         socket.on("newIceCandidate", (message) => {
-            console.log("OKT")
-
             io.emit("iceCandidate", message)
         })
 
-
         socket.on("answer", (message) => {
-            console.log("answer")
-
             io.emit("message", message)
         })
 
         socket.on("offer", (message) => {
-            console.log("offer")
-
             io.emit("messageoffer", message)
         })
-        
-
+    
     });
 }
